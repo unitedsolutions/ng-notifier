a8a97485fc45a09b15674842a7391e7e8bf2ee49
daretobebetter <30729632+daretobebetter@users.noreply.github.com>
2019-03-19 12:12:19 -0400

    Delete ng-us-notifier.js

--- dist/ng-us-notifier.js
+++
@@ -1,6 +0,0 @@
-export { NotifierComponentsModule } from './components/notifier.components.module';
-export { SimpleNotifierComponent } from './components/simple-notifier/simple-notifier.component';
-export { ActionsNotifierComponent } from './components/actions-notifier/actions-notifier.component';
-export { NotifierServicesModule } from './services/notifier.services.module';
-export { Notifier } from './services/notifier.service';
-//# sourceMappingURL=ng-us-notifier.js.map
